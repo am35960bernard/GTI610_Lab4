46b7c0dcd0a536f5750fee69db7fbaf02f83f212
am35960bernard <bernard.millet.1@etsmtl.net>
2017-06-22 10:24:49 -0400

    Start socket

---
+++ src/serveur/Server.java
@@ -0,0 +1,28 @@
+package serveur;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+	public static void main(String[] zero) {
+		
+		ServerSocket socketserver  ;
+		Socket socketduserveur ;
+		
+		try {
+			
+			socketserver = new ServerSocket(2222);
+			socketduserveur = socketserver.accept(); 
+			System.out.println("Un zéro s'est connecté !");
+		        socketserver.close();
+		        socketduserveur.close();
+
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
