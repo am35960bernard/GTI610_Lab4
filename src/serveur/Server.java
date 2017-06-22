921682fdec967f6e00371549581be2b2fdc865c3
am35960bernard <bernard.millet.1@etsmtl.net>
2017-06-22 10:41:15 -0400

    Thread ready

--- src/serveur/Server.java
+++ src/serveur/Server.java
@@ -1,28 +1,47 @@
 package serveur;
-
 import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.*;
 
 public class Server {
 
-	public static void main(String[] zero) {
+	public static void main(String[] zero){
 		
-		ServerSocket socketserver  ;
-		Socket socketduserveur ;
-		
+		ServerSocket socket;
 		try {
+		socket = new ServerSocket(2222);
+		Thread t = new Thread(new Accepter_clients(socket));
+		t.start();
+		System.out.println("Mes employeurs sont prêts !");
+		
+		} catch (IOException e) {
 			
-			socketserver = new ServerSocket(2222);
-			socketduserveur = socketserver.accept(); 
-			System.out.println("Un zéro s'est connecté !");
-		        socketserver.close();
-		        socketduserveur.close();
-
-		}catch (IOException e) {
 			e.printStackTrace();
 		}
-		
 	}
-
 }
+
+class Accepter_clients implements Runnable {
+
+	   private ServerSocket socketserver;
+	   private Socket socket;
+	   private int nbrclient = 1;
+		public Accepter_clients(ServerSocket s){
+			socketserver = s;
+		}
+		
+		public void run() {
+
+	        try {
+	        	while(true){
+			  socket = socketserver.accept(); // Un client se connecte on l'accepte
+	                  System.out.println("Le client numéro "+nbrclient+ " est connecté !");
+	                  nbrclient++;
+	                  socket.close();
+	        	}
+	        
+	        } catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
