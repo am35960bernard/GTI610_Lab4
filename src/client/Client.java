d3c83886f5745d348fc425080a69637c4189fe0d
Hai-Dang Ly <AM55130@ens.etsmtl.ca>
2017-06-22 12:06:55 -0400

    Pour recevoir un message du serveur

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,29 +1,54 @@
 package client;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
+//http://www.careerbless.com/samplecodes/java/beginners/socket/SocketBasic1.php
 
 public class Client {
 	
 	public static void main(String[] zero) {
 		
 		Socket socket;
+		String message = "We come in piece.";
 
 		try {
 		
-		     socket = new Socket(InetAddress.getLocalHost(),2222);	
-	             socket.close();
-
+			//Connecter au serveur
+		    socket = new Socket(InetAddress.getLocalHost(),2222);	
+		    
+		    //envoyer un message au serveur
+		    OutputStream os = socket.getOutputStream();
+            OutputStreamWriter osw = new OutputStreamWriter(os);
+            BufferedWriter bw = new BufferedWriter(osw);
+ 
+            String envoieMessage = message + "\n";
+            bw.write(envoieMessage);
+            bw.flush();
+            
+            //recevoir le message retourner du serveur
+            InputStream is = socket.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+            String messageRecu = br.readLine();
+            System.out.println("Message retourner par le serveur : " +messageRecu);
+            
+			socket.close();
+			
 		}catch (UnknownHostException e) {
 			
 			e.printStackTrace();
 		}catch (IOException e) {
 			
 			e.printStackTrace();
-		}
+		}	
 	}
-
 }
--- src/serveur/Server.java
+++ src/serveur/Server.java
@@ -1,47 +1,76 @@
 package serveur;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
-import java.net.*;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.net.ServerSocket;
 
 public class Server {
 
 	public static void main(String[] zero){
-		
+
 		ServerSocket socket;
+		
 		try {
 		socket = new ServerSocket(2222);
 		Thread t = new Thread(new Accepter_clients(socket));
 		t.start();
-		System.out.println("Mes employeurs sont prêts !");
+		System.out.println("Bring it on 2222!");
 		
 		} catch (IOException e) {
-			
 			e.printStackTrace();
 		}
 	}
 }
 
 class Accepter_clients implements Runnable {
-
-	   private ServerSocket socketserver;
-	   private Socket socket;
-	   private int nbrclient = 1;
-		public Accepter_clients(ServerSocket s){
-			socketserver = s;
-		}
+		
+	private int MAX = 256;
+	private ServerSocket socketserver;
+	private Socket socket;
+	private int nbrclient = 1;
+	private byte[] buff = new byte[MAX];
+	
+	private String messageRetour = "There will be no peace" + "\n";
+	 
+	public Accepter_clients(ServerSocket s){
+		socketserver = s;
+	}
 		
 		public void run() {
-
-	        try {
+	        
+			try {
 	        	while(true){
-			  socket = socketserver.accept(); // Un client se connecte on l'accepte
-	                  System.out.println("Le client numéro "+nbrclient+ " est connecté !");
-	                  nbrclient++;
-	                  socket.close();
+	        		//accepter la demande de connexion du client 
+	        		socket = socketserver.accept(); // Un client se connecte on l'accepte
+	                System.out.println("Le client numéro "+nbrclient+ " est connecté !");
+	                nbrclient++;
+	                
+	                //recevoir le message du client
+	                InputStream is = socket.getInputStream();
+	                InputStreamReader isr = new InputStreamReader(is);
+	                BufferedReader br = new BufferedReader(isr);
+	                String message = br.readLine();
+	                System.out.println("Message reçu du client: "+message);
+	        		
+	           
+	                //envoyer le message de retour au client.
+	                OutputStream os = socket.getOutputStream();
+	                OutputStreamWriter osw = new OutputStreamWriter(os);
+	                BufferedWriter bw = new BufferedWriter(osw);
+	                bw.write(messageRetour);
+	                System.out.println("Message retourner au client: "+messageRetour);
+	                bw.flush();
+	                
+	                socket.close();
 	        	}
 	        
 	        } catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
-
 	}
